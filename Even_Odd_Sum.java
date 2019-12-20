93b67b7d107ea185bd6c75ca5825ddc963cd7be8
VimalKarthik3112 <59052713+VimalKarthik3112@users.noreply.github.com>
2019-12-20 02:53:50 -0800

    Create Even_Odd_Sum.java

---
+++ Even_Odd_Sum.java
@@ -0,0 +1,28 @@
+//Summing of odd and even intergers
+
+
+import java.util.Scanner;
+class Main {
+  
+  public static void main(String[] args)
+  {
+Scanner scan=new Scanner(System.in);
+int a,b,es=0,os=0,i;
+for(i=1;i>=i;i++)
+{
+  System.out.println("Enter the number: ");
+  a=scan.nextInt();
+  if(a%2==0)
+  {
+    es=es+a;
+    System.out.println("Sum of even "+es);
+  }
+  else
+  {
+     os=os+a;
+    System.out.println(" Sum of odd "+os);
+  }
+}
+
+  }
+  }
